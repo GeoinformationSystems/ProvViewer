6ed82525c78f38e56b7cd705a038d87232f64e99
rue_a <arne.ruemmler@gmail.com>
2021-03-23 14:31:56 +0100

    add controls

--- src/main/java/Prov2Graph.java
+++ src/main/java/Prov2Graph.java
@@ -1,4 +1,5 @@
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
@@ -11,6 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.net.URLDecoder;
 
+import org.apache.jena.rdfconnection.*;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.Lang;
+import java.io.FileOutputStream;
+
 @WebServlet(name = "Prov2Graph")
 
 public class Prov2Graph extends HttpServlet {
@@ -19,62 +27,285 @@ public class Prov2Graph extends HttpServlet {
      */
     private static final long serialVersionUID = 1L;
 
+    public static Document ttl2xml (String serviceURI) throws IOException {
+
+		String dataQuery = "SELECT ?s WHERE {?s a <http://www.w3.org/ns/prov#Entity>}";
+		String processQuery = "SELECT ?s WHERE {?s a <http://www.w3.org/ns/prov#Activity>}";
+		String generatedQuery = "SELECT ?s ?o WHERE {?s <http://www.w3.org/ns/prov#wasGeneratedBy> ?o}";
+		String usedQuery = "SELECT ?s ?o WHERE {?s <http://www.w3.org/ns/prov#used> ?o}";
+		String agentQuery = "SELECT ?s WHERE {?s a <http://www.w3.org/ns/prov#Agent>}";
+		String attributedQuery = "SELECT ?s ?o WHERE {?s <http://www.w3.org/ns/prov#wasAttributedTo> ?o}";
+		String associatedQuery = "SELECT ?s ?o WHERE {?s <http://www.w3.org/ns/prov#wasAssociatedWith> ?o}";
+
+
+		Document document = new Document();
+        Element mxGraphModel = new Element("mxGraphModel");
+
+        Element root = new Element("root");
+
+        Element diagram = new Element("Diagram")
+            .setAttribute("label", "My Diagram")
+            .setAttribute("href", "http://www.jgraph.com/")
+            .setAttribute("id", "0");
+        diagram.addContent(new Element("mxCell"));
+
+        Element layer = new Element("Layer")
+            .setAttribute("label", "Default Layer")
+            .setAttribute("id", "1");
+        layer.addContent(
+            new Element("mxCell")
+                .setAttribute("parent", "0"));
+
+        root.addContent(diagram)
+            .addContent(layer);
+
+
+		try (QueryExecution dataQ = QueryExecutionFactory.sparqlService(serviceURI, dataQuery)) {
+			ResultSet dataResults = dataQ.execSelect();
+
+			while (dataResults.hasNext()) {
+				QuerySolution soln = dataResults.nextSolution();
+				RDFNode x = soln.get("s");
+			
+				String o = x.toString();
+				int oLength = o.length() * 6 + 10;
+				Element dataDMP = new Element("DataDMP")
+					.setAttribute("label", o)
+					.setAttribute("id", o)
+					.addContent(new Element("mxCell")
+						.setAttribute("style", "dataDMP")
+						.setAttribute("parent", "1")
+						.setAttribute("vertex", "1")                
+					.addContent(new Element("mxGeometry")
+						.setAttribute("x", "0")
+						.setAttribute("y", "0")
+						.setAttribute("width", String.valueOf(oLength))
+						.setAttribute("height", "40")
+						.setAttribute("as", "geometry")));
+				root.addContent(dataDMP);				
+			}
+		}
+			
+		try (QueryExecution processQ = QueryExecutionFactory.sparqlService(serviceURI, processQuery)) {
+			ResultSet processResults = processQ.execSelect();
+			while (processResults.hasNext()) {
+				QuerySolution soln = processResults.nextSolution();
+				RDFNode x = soln.get("s");
+	
+				String s = x.toString();
+				int sLength = s.length()*5 + 10;
+				Element processDMP = new Element ("ProcessDMP")
+				.setAttribute("label", s)
+				.setAttribute("id", s)
+				.addContent(new Element ("mxCell")
+					.setAttribute("style", "processDMP")
+					.setAttribute("vertex", "1")
+					.setAttribute("parent", "1")
+				.addContent(new Element("mxGeometry")
+					.setAttribute("x", "100")
+					.setAttribute("y", "0")
+					.setAttribute("width", String.valueOf(sLength))
+					.setAttribute("height", "40")
+					.setAttribute("as", "geometry")));
+				root.addContent(processDMP);				
+			}
+		}
+
+		try (QueryExecution agentQ = QueryExecutionFactory.sparqlService(serviceURI, agentQuery)) {
+			ResultSet agentResults = agentQ.execSelect();
+			while (agentResults.hasNext()) {
+				QuerySolution soln = agentResults.nextSolution();
+				RDFNode x = soln.get("s");
+	
+				String s = x.toString();
+				int sLength = s.length()*5+5;
+				Element agentDMP = new Element ("AgentDMP")
+				.setAttribute("label", s)
+				.setAttribute("id", s)
+				.addContent(new Element ("mxCell")
+					.setAttribute("style", "agentDMP")
+					.setAttribute("vertex", "1")
+					.setAttribute("parent", "1")
+				.addContent(new Element("mxGeometry")
+					.setAttribute("x", "100")
+					.setAttribute("y", "0")
+					.setAttribute("width", String.valueOf(sLength))
+					.setAttribute("height", "40")
+					.setAttribute("as", "geometry")));
+				root.addContent(agentDMP);				
+			}
+		}
+
+		try (QueryExecution generatedQ = QueryExecutionFactory.sparqlService(serviceURI, generatedQuery)) {
+			ResultSet generatedResults = generatedQ.execSelect();
+			int id = 2;
+			while (generatedResults.hasNext()) {
+				QuerySolution soln = generatedResults.nextSolution();
+				
+				RDFNode s = soln.get("s");
+				RDFNode o = soln.get("o");
+
+				String subj = s.toString();
+				String obj = o.toString();
+
+				Element connector = new Element ("Connector")
+				// .setAttribute("label", "wasGeneratedBy")
+				.setAttribute("id", String.valueOf(id))
+				.addContent(new Element ("mxCell")
+					.setAttribute("style", "noEdgeStyle=1;orthogonal=1;")
+					.setAttribute("edge", "1")
+					.setAttribute("parent", "1")
+					.setAttribute("source", obj)
+					.setAttribute("target", subj)
+					.addContent(new Element ("mxGeometry")
+						.setAttribute("relative", "1")
+						.setAttribute("as", "geometry")
+						.addContent(new Element ("Array")
+							.setAttribute("as", "geometry")
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0"))
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0")))));
+				root.addContent(connector);
+				id++;
+			}
+		}
+
+		try (QueryExecution usedQ = QueryExecutionFactory.sparqlService(serviceURI, usedQuery)) {
+			ResultSet usedResults = usedQ.execSelect();
+			int id = 2;
+			while (usedResults.hasNext()) {
+				QuerySolution soln = usedResults.nextSolution();
+				
+				RDFNode s = soln.get("s");
+				RDFNode o = soln.get("o");
+
+				String subj = s.toString();
+				String obj = o.toString();
+
+				Element connector = new Element ("Connector")
+				// .setAttribute("label", "used")
+				.setAttribute("id", String.valueOf(id) + "b" )
+				.addContent(new Element ("mxCell")
+					.setAttribute("style", "noEdgeStyle=1;orthogonal=1;")
+					.setAttribute("edge", "1")
+					.setAttribute("parent", "1")
+					.setAttribute("source", obj)
+					.setAttribute("target", subj)
+					.addContent(new Element ("mxGeometry")
+						.setAttribute("relative", "1")
+						.setAttribute("as", "geometry")
+						.addContent(new Element ("Array")
+							.setAttribute("as", "geometry")
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0"))
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0")))));
+				root.addContent(connector);
+				id++;
+			}
+		}	
+
+		try (QueryExecution associatedQ = QueryExecutionFactory.sparqlService(serviceURI, associatedQuery)) {
+			ResultSet associatedResults = associatedQ.execSelect();
+			int id = 2;
+			while (associatedResults.hasNext()) {
+				QuerySolution soln = associatedResults.nextSolution();
+				
+				RDFNode s = soln.get("s");
+				RDFNode o = soln.get("o");
+
+				String subj = s.toString();
+				String obj = o.toString();
+
+				Element connector = new Element ("Connector")
+				// .setAttribute("label", "wasAssociatedWith")
+				.setAttribute("id", String.valueOf(id) + "c" )
+				.addContent(new Element ("mxCell")
+					.setAttribute("style", "noEdgeStyle=1;orthogonal=1;")
+					.setAttribute("edge", "1")
+					.setAttribute("parent", "1")
+					.setAttribute("source", subj)
+					.setAttribute("target", obj)
+					.addContent(new Element ("mxGeometry")
+						.setAttribute("relative", "1")
+						.setAttribute("as", "geometry")
+						.addContent(new Element ("Array")
+							.setAttribute("as", "geometry")
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0"))
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0")))));
+				root.addContent(connector);
+				id++;
+			}
+		}
+
+		try (QueryExecution attributedQ = QueryExecutionFactory.sparqlService(serviceURI, attributedQuery)) {
+			ResultSet attributedResults = attributedQ.execSelect();
+			int id = 2;
+			while (attributedResults.hasNext()) {
+				QuerySolution soln = attributedResults.nextSolution();
+				
+				RDFNode s = soln.get("s");
+				RDFNode o = soln.get("o");
+
+				String subj = s.toString();
+				String obj = o.toString();
+
+				Element connector = new Element ("Connector")
+				// .setAttribute("label", "wasAttributedTo")
+				.setAttribute("id", String.valueOf(id) + "d" )
+				.addContent(new Element ("mxCell")
+					.setAttribute("style", "noEdgeStyle=1;orthogonal=1;")
+					.setAttribute("edge", "1")
+					.setAttribute("parent", "1")
+					.setAttribute("source", subj)
+					.setAttribute("target", obj)
+					.addContent(new Element ("mxGeometry")
+						.setAttribute("relative", "1")
+						.setAttribute("as", "geometry")
+						.addContent(new Element ("Array")
+							.setAttribute("as", "geometry")
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0"))
+							.addContent(new Element ("mxPoint")
+								.setAttribute("x", "0")
+								.setAttribute("y", "0")))));
+				root.addContent(connector);
+				id++;
+			}
+		}
+		
+		mxGraphModel.addContent(root);
+        document.setContent(mxGraphModel);
+        return document;
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        if (request.getContentLength() < Constants.MAX_REQUEST_SIZE) {
-            String filename = request.getParameter("filename");
-            String xml = request.getParameter("xml");
-
-            if (filename == null) {
-                filename = "export";
-            }
-
-            if (xml != null && xml.length() > 0) {
-                String format = request.getParameter("format");
-
-                if (format == null) {
-                    format = "xml";
-                }
-
-                if (!filename.toLowerCase().endsWith("." + format)) {
-                    filename += "." + format;
-                }
-
-                // Decoding is optional (no plain text values allowed)
-                if (xml != null && xml.startsWith("%3C")) {
-                    xml = URLDecoder.decode(xml, "UTF-8");
-                }
-
                 try {
-                    Document doc = new SAXBuilder().build(new StringReader(xml));
-
-                    // Source xmlFile = new JDOMSource(doc);
-                    // JDOMResult provResult = new JDOMResult();
-                    // Transformer transformer = TransformerFactory.newInstance().newTransformer(new
-                    // StreamSource(
-                    // String.valueOf(getClass().getClassLoader().getResource("Prov2Graph.xsl"))));
-                    // transformer.transform(xmlFile, provResult);
+                    Document doc = ttl2xml("http://172.26.62.72:8080/fuseki/provenance_graph_example");
 
                     response.setContentType("text/plain");
                     response.setHeader("Content-Disposition",
-                            "attachment; filename=\"" + filename + "\"; filename*=UTF-8''" + filename);
+                            "attachment");
                     response.setStatus(HttpServletResponse.SC_OK);
                     OutputStream out = response.getOutputStream();
                     new XMLOutputter(Format.getPrettyFormat()).output(doc, out);
-                    // new XMLOutputter(Format.getPrettyFormat()).output(provResult.getDocument(),
-                    // out);
                     out.flush();
                     out.close();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-            } else {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             }
-        } else {
-            response.setStatus(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE);
-        }
-    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
---
+++ src/main/webapp/js/functionality.js
@@ -0,0 +1,213 @@
+
+$(document).ready(function(){
+    $("#getDatasets").bind("click", function () {    
+        var endpointUrl = 'http://172.26.62.72:8080/fuseki/provenance_graph_example/query',
+            sparqlQuery =  [
+                "PREFIX prov: <http://www.w3.org/ns/prov#>",
+                "PREFIX dct: <http://purl.org/dc/terms/>",
+                "SELECT ?entity ?entityTitle WHERE {",
+                    "?entity a prov:Entity .",
+                    "?entity dct:title ?entityTitle .",
+                "}"
+            ].join(" ");
+            settings = {
+                headers: { Accept: 'application/sparql-results+json' },
+                data: { query: sparqlQuery }
+            };
+        
+            $.ajax( endpointUrl, settings ).then( function ( data ) {
+                // $( 'body' ).append( ( $('<pre>').text( JSON.stringify( data) ) ) );
+
+                let results = data["results"]["bindings"];
+                let select = document.getElementById("selectionBox");
+                for (let i = 0; i < results.length; i++) {
+                    option = document.createElement("option")
+                    option.value = (results[i]["entity"]["value"]);
+                    option.text = (results[i]["entityTitle"]["value"]);
+                    selectionBox.add(option);
+                    
+                }
+            });
+        });
+    });
+    function onInit(editor) {
+        $("#addToCanvas").bind("click", function (e) {
+            $.ajax({
+                type: 'POST',
+                url: "./importPROV",
+                data: {
+                    'filename': 'exportPROV',
+                    'xml': escape(e.target.result)
+                },
+                success: function (data) {
+                    if (data == null)
+                        return;
+
+                    $('#myModal').modal('hide');
+                    editor.graph.getModel().beginUpdate();
+                    console.log(data)
+                    try {
+                        editor.graph.removeCells(editor.graph.getChildCells(editor.graph.getDefaultParent(), true, true));
+                        var root = mxUtils.parseXml(data).documentElement;
+                        var dec = new mxCodec(root.ownerDocument);
+                        dec.decode(root, editor.graph.getModel());
+                    }
+                    finally {
+                        // Updates the display and fit the graph
+                        editor.graph.getModel().endUpdate();
+                        var layout = new mxHierarchicalLayout(editor.graph, mxConstants.DIRECTION_WEST, true);
+                        layout.interHierarchySpacing = 30;
+                        layout.interRankCellSpacing = 30;
+                        layout.intraCellSpacing = 30;
+                        var parent = editor.graph.getDefaultParent();
+                        layout.execute(parent);
+                        editor.graph.fit();
+                        editor.graph.view.rendering = true;
+                        editor.graph.refresh();
+                    }
+                }
+            });
+                    
+             
+        });
+
+        mxVertexHandler.prototype.rotationEnabled = false;
+        mxGraphHandler.prototype.guidesEnabled = true;
+        mxGuide.prototype.isEnabledForEvent = function (evt) {
+            return !mxEvent.isAltDown(evt);
+        };
+
+        mxConnectionHandler.prototype.connectImage = new mxImage('images/DMP/right-arrow.png', 16, 16);
+
+        editor.graph.setConnectable(false);
+        editor.graph.setConnectableEdges(false);
+        editor.graph.setAllowDanglingEdges(false);
+        editor.graph.setDisconnectOnMove(false);
+
+        editor.graph.multiplicities.push(new mxMultiplicity(true, 'DataDMP', null, null, 'n', 'n', ['DataDMP'],
+            'Count Error data data',
+            'Connection from Data to Data not allowed', false));
+        editor.graph.multiplicities.push(new mxMultiplicity(true, 'ProcessDMP', null, null, 'n', 'n', ['ProcessDMP'],
+            'Count Error process Process',
+            'Connection from Process to Process not allowed', false));
+        editor.graph.multiplicities.push(new mxMultiplicity(false, 'ActorDMP', null, null, 0, 0, null,
+            'Actor must have NO incoming edge',
+            null));
+
+        editor.validating = true;
+
+        var title = document.getElementById('title');
+
+        if (title != null) {
+            var f = function (sender) {
+                title.innerHTML = 'mxDraw - ' + sender.getTitle();
+            };
+
+            editor.addListener(mxEvent.ROOT, f);
+            f(editor);
+        }
+
+        mxEvent.addMouseWheelListener(function (evt, up) {
+            if (!mxEvent.isConsumed(evt)) {
+                if (up) {
+                    editor.execute('zoomIn');
+                }
+                else {
+                    editor.execute('zoomOut');
+                }
+
+                mxEvent.consume(evt);
+            }
+        });
+        mxEvent.addListener(document.getElementById("plusButton"), 'click', function () {
+            editor.execute('zoomIn');
+
+        });
+        mxEvent.addListener(document.getElementById("minusButton"), 'click', function () {
+            editor.execute('zoomOut');
+        });
+        mxEvent.addListener(document.getElementById("fitButton"), 'click', function () {
+            editor.execute('fit')
+        });
+        mxEvent.addListener(document.getElementById("hierarchicalButton"), 'click', function () {
+            var layout = new mxHierarchicalLayout(editor.graph, mxConstants.DIRECTION_WEST, true);
+            layout.interHierarchySpacing = 30;
+            layout.interRankCellSpacing = 30;
+            layout.intraCellSpacing = 30;
+            var parent = editor.graph.getDefaultParent();
+            layout.execute(parent);
+            editor.graph.fit();
+            editor.graph.view.rendering = true;
+            editor.graph.refresh();
+        });
+
+        editor.graph.addListener(mxEvent.LABEL_CHANGED, function (sender, evt) {
+            editor.graph.refresh();
+            editor.graph.updateCellSize(evt.properties.cell);
+        });
+
+        var graphGetPreferredSizeForCell = editor.graph.getPreferredSizeForCell;
+        editor.graph.getPreferredSizeForCell = function (cell) {
+            var result = graphGetPreferredSizeForCell.apply(this, arguments);
+            var style = this.getCellStyle(cell);
+
+            if (style['minHeight'] > 0) {
+                result.height = Math.max(style['minHeight'], result.height);
+            }
+            if (style['minWidth'] > 0) {
+                result.width = Math.max(style['minWidth'], result.width);
+                if (style['shape'] == "dataDMP") {
+                    result.width += 50;
+                }
+            }
+            return result;
+        };
+
+        editor.graph.connectionHandler.addListener(mxEvent.CONNECT, function (sender, evt) {
+            var edge = evt.getProperty('cell');
+            var source = editor.graph.getModel().getTerminal(edge, true);
+
+            if (source.getStyle() == "actorDMP") {
+
+                var style = editor.graph.getCellStyle(edge);
+                var newStyle = editor.graph.stylesheet.getCellStyle("edgeStyle=elbowEdgeStyle;html=1;dashed=1;rounded=1;jettySize=auto;orthogonalLoop=1;strokeColor=#727675;strokeWidth=1;endArrow=openThin;startArrow=oval;", style); //Method will merge styles into a new style object.  We must translate to string from here
+                var array = [];
+                for (var prop in newStyle)
+                    array.push(prop + "=" + newStyle[prop]);
+                edge.style = array.join(';');
+            }
+        });
+
+
+
+        editor.graph.flipEdge = function (edge) {
+            if (edge != null) {
+                var state = this.view.getState(edge);
+                var style = (state != null) ? state.style : this.getCellStyle(edge);
+
+                if (style != null) {
+                    var elbow = mxUtils.getValue(style, mxConstants.STYLE_ELBOW,
+                        mxConstants.ELBOW_HORIZONTAL);
+                    var value = (elbow == mxConstants.ELBOW_HORIZONTAL) ?
+                        mxConstants.ELBOW_VERTICAL : mxConstants.ELBOW_HORIZONTAL;
+                    this.setCellStyles(mxConstants.STYLE_ELBOW, value, [edge]);
+                }
+            }
+        };
+
+        editor.addAction('editMetadata', function (editor, cell) {
+            console.log(editor);
+            console.log(cell);
+
+            if (cell != null) {
+                var dlg = new EditDataDialog(editor, cell);
+                $('#editMetadataModal').modal('show');
+                // ui.showDialog(dlg.container, 320, 320, true, false);
+                // dlg.init();
+            }
+        })
+    }
+
+    window.onbeforeunload = function () {
+        return mxResources.get('changesLost');
+    };
